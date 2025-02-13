e807683ed2e983eb702198dbda9cf2de4ed77208
Mai Cường Thọ <thomc@ntu.edu.vn>
2025-02-13 11:10:19 +0700

    Full code. test OK

--- ViDuNhapXuat1/src/ntu/maicuongtho/CTTinhTong.java
+++ ViDuNhapXuat1/src/ntu/maicuongtho/CTTinhTong.java
@@ -1,10 +1,27 @@
 package ntu.maicuongtho;
 
+import java.util.Scanner;
+
 public class CTTinhTong {
 
 	public static void main(String[] args) {
+		//In ra một dòng tiêu đề App
+		System.out.print("CT TINH TONG \n");
 		
-
+		//Chuẩn bị cho việc nhập
+		Scanner banPhim = new Scanner(System.in);
+		
+		//In dong huong nhan Moi nhap A
+		System.out.println("Mời nhập a: ");
+		double a= banPhim.nextDouble();
+		System.out.println("Mời nhập b: ");
+		double b= banPhim.nextDouble();
+		// Tín toán
+		double tong = a+b;
+		//
+		System.out.print("Tổng là: ");
+		System.out.print(tong);
+		
 	}
 
 }
